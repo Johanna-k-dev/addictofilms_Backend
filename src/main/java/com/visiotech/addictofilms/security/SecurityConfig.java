c0e1668b1e420b9a5f190760af86af6affb11720
Johanna Keïta <veille.technologique.jk@gmail.com>
2025-02-07 14:40:54 +0100

    Added routes  security

--- src/main/java/com/visiotech/addictofilms/security/SecurityConfig.java
+++ src/main/java/com/visiotech/addictofilms/security/SecurityConfig.java
@@ -1,20 +1,85 @@
 package com.visiotech.addictofilms.security;
 
+import com.visiotech.addictofilms.services.CustomUserDetailsService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
-    // @Bean
-    // public CorsConfigurationSource corsConfigurationSource() {
-    //     CorsConfiguration configuration = new CorsConfiguration();
-    //     configuration.setAllowedOrigins(List.of("http://localhost:3000")); // Origines autorisées
-    //     configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Méthodes autorisées
-    //     configuration.setAllowedHeaders(List.of("Authorization", "Content-Type")); // Headers autorisés
-    //     configuration.setAllowCredentials(true); // Permet l'authentification avec des cookies/token
-    //
-    //     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    //     source.registerCorsConfiguration("/**", configuration); // Appliquer à toutes les routes
-    //     return source;
-    // }
+   private final JwtFilter jwtFilter;
+   private final CustomUserDetailsService userDetailsService;
+
+   public SecurityConfig(JwtFilter jwtFilter,CustomUserDetailsService userDetailsService){
+       this.jwtFilter = jwtFilter;
+       this.userDetailsService = userDetailsService;
+   }
+
+   @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+       http
+               .csrf(csrf -> csrf.disable())
+               .cors(Customizer.withDefaults())
+               .sessionManagement(sessionManagment -> sessionManagment.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+               .authorizeHttpRequests(authorizeRequests ->
+                       authorizeRequests
+                               .requestMatchers("/auth/**","/test/all").permitAll()
+                               .requestMatchers("/user/**").hasRole("USER")
+                               .anyRequest().authenticated()
+               );
+       http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+       return http.build();
+   }
+
+   @Bean
+    public AuthenticationProvider authenticationProvider(){
+       DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+       provider.setUserDetailsService(userDetailsService);
+       provider.setPasswordEncoder(new BCryptPasswordEncoder());
+       return provider;
+   }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration
+    ) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(List.of("localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
 }
